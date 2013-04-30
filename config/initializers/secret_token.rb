9b0ec3dc62339fb1136f8a0dda7213ba47af7b13
Wen-Lung Yinn <dragon.yinn@gmail.com>
2013-04-30 10:22:41 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryBooks::Application.config.secret_token = 'e1f6eae53ea46ba68f5e5cc21b905cc111cafc62f1fdf1761bd0bbd833c6f43c3711d636a58c78504a43c7a3d743a75ff9f31c059e842504fbda52c020267251'
